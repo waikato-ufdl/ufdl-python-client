13bf37e4e0d490f0eadf0efcde6d4a2ac135dfb0
csterling <coreytsterling@gmail.com>
2019-11-20 09:46:30 +1300

    Added comments.

--- src/ufdl/pythonclient/core/_context.py
+++ src/ufdl/pythonclient/core/_context.py
@@ -1,7 +1,6 @@
 """
 Core module containing the common context for all calls made by the API.
-This is the host/port the UFDL API is listening to, and the username/password
-to use for authentication.
+Currently this is just the host/port the UFDL API server is listening to.
 """
 # The context
 _host: str = ""
@@ -9,14 +8,26 @@ _port: int = 0
 
 
 def set_context(host: str, port: int):
+    """
+    Sets the context for the client.
+
+    :param host:    The UFDL server host.
+    :param port:    The UFDL server port.
+    """
     global _host, _port
     _host = host
     _port = port
 
 
 def get_host() -> str:
+    """
+    Gets the UFDL server host set in the context.
+    """
     return _host
 
 
 def get_port() -> int:
+    """
+    Gets the UFDL server port set in the context.
+    """
     return _port
