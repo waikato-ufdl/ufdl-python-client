a44ddbffba8d3ef74e82f61bae2fe61f7ad02d0f
csterling <coreytsterling@gmail.com>
2019-11-20 09:47:49 +1300

    Added comments.

--- src/ufdl/pythonclient/core/__init__.py
+++ src/ufdl/pythonclient/core/__init__.py
@@ -1,2 +1,7 @@
+"""
+Core package for the UFDL client library. Provides the ability to set
+the communication context (host/port to communicate with) and functions
+for performing the communication.
+"""
 from ._context import set_context, get_host, get_port
 from ._methods import get, post, put, patch, delete
