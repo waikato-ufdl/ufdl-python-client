3edb11d4721f516e7f6235891b7901386b51feb2
Peter Reutemann <fracpete@gmail.com>
2019-11-28 15:35:56 +1300

    updated URLs

--- src/ufdl/pythonclient/constants.py
+++ src/ufdl/pythonclient/constants.py
@@ -3,9 +3,9 @@
 # ---- #
 
 # JWT
-JWT_OBTAIN_TOKEN_URL = "jwt/obtain/"  # URL to obtain a new JWT token pair
-JWT_REFRESH_TOKEN_URL = "jwt/refresh/"  # URL to refresh the JWT access token
-JWT_VERIFY_TOKEN_URL = "jwt/verify/"  # URL to verify a JWT token
+JWT_OBTAIN_TOKEN_URL = "v1/auth/obtain/"  # URL to obtain a new JWT token pair
+JWT_REFRESH_TOKEN_URL = "v1/auth/refresh/"  # URL to refresh the JWT access token
+JWT_VERIFY_TOKEN_URL = "v1/auth/verify/"  # URL to verify a JWT token
 
 # CORE
 ORGANISATIONS_URL = "core/organisations/"
