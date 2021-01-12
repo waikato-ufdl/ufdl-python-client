ebeacde12b0690da8876bc19dc39da126ee4f51f
csterling <coreytsterling@gmail.com>
2021-01-13 10:51:01 +1300

    Spaced out parameters.

--- src/ufdl/pythonclient/functional/image_classification/_mixin_actions.py
+++ src/ufdl/pythonclient/functional/image_classification/_mixin_actions.py
@@ -9,13 +9,29 @@ from ..._UFDLServerContext import UFDLServerContext
 # ================= #
 
 
-def get_categories(context: UFDLServerContext, url: str, pk: int) -> RawJSONObject:
+def get_categories(
+        context: UFDLServerContext,
+        url: str,
+        pk: int
+) -> RawJSONObject:
     return context.get(f"{url}/{pk}/categories").json()
 
 
-def add_categories(context: UFDLServerContext, url: str, pk: int, images: List[str], categories: List[str]) -> RawJSONObject:
+def add_categories(
+        context: UFDLServerContext,
+        url: str,
+        pk: int,
+        images: List[str],
+        categories: List[str]
+) -> RawJSONObject:
     return context.patch(f"{url}/{pk}/categories", {"method": "add", "images": images, "categories": categories}).json()
 
 
-def remove_categories(context: UFDLServerContext, url: str, pk: int, images: List[str], categories: List[str]) -> RawJSONObject:
+def remove_categories(
+        context: UFDLServerContext,
+        url: str,
+        pk: int,
+        images: List[str],
+        categories: List[str]
+) -> RawJSONObject:
     return context.patch(f"{url}/{pk}/categories", {"method": "remove", "images": images, "categories": categories}).json()
