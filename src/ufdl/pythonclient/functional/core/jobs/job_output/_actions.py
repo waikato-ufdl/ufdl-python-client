0fb4700e9aac7b489fc75dcd77d774fe9eb7d283
csterling <coreytsterling@gmail.com>
2021-03-24 11:01:36 +1300

    Added the download action to job outputs.

--- src/ufdl/pythonclient/functional/core/jobs/job_output/_actions.py
+++ src/ufdl/pythonclient/functional/core/jobs/job_output/_actions.py
@@ -1,9 +1,23 @@
+from typing import Iterator
+
 from wai.json.raw import RawJSONObject
 
 from .....constants import JOB_OUTPUTS_URL
 from ....._UFDLServerContext import UFDLServerContext
 from .... import _base_actions
+from ... import _mixin_actions
 
 
 def retrieve(context: UFDLServerContext, pk: int) -> RawJSONObject:
     return _base_actions.retrieve(context, JOB_OUTPUTS_URL, pk)
+
+
+def download(
+        context: UFDLServerContext,
+        pk: int
+) -> Iterator[bytes]:
+    return _mixin_actions.download(
+        context,
+        JOB_OUTPUTS_URL,
+        pk
+    )
